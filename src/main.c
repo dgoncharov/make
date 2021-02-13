bd0d6051ab2cc84948af5133c00020e9747004d4
Dmitry Goncharov <dgoncharov@users.sf.net>
2021-02-13 10:05:02 -0500

    Add notintermediate to .FEATURES.

--- src/main.c
+++ src/main.c
@@ -1327,7 +1327,7 @@ main (int argc, char **argv, char **envp)
   {
     const char *features = "target-specific order-only second-expansion"
                            " else-if shortest-stem undefine oneshell nocomment"
-                           " grouped-target extra-prereqs"
+                           " grouped-target extra-prereqs notintermediate"
 #ifndef NO_ARCHIVES
                            " archives"
 #endif
